d7fd0df316a345f1c0ddebd7551d706b8d2ea34c
L D <tapircode@gmail.com>
2019-03-12 03:19:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,21 +6,23 @@ end
 
 def summon_captain_planet(names)
   result = []
-  for names.each do |name|
+  names.each do |name|
     result <<  string.slice(0,1).capitalize + string.slice(1..-1) + "!"
   end
   return result
 end
 
 def long_planeteer_calls(words)
-  for words.each do |word|
+  words.each do |word|
     if word.length > 4
       return true
   end
   return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.each do |cheese|
+    
+  end
 end
